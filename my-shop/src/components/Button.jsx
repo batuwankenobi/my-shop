80757c36ed5a2719e3cfe96484947201a072f580
batu <batuhanbartuu@gmail.com>
2025-02-11 21:46:54 +0300

    eslint for buttonjsx

---
+++ my-shop/src/components/Button.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line react/prop-types
+function Button({text, onClick}) {
+
+	return (
+  
+	    <button
+		  className="bg-[#23A6F0] text-white  py-4 px-8  transition-transform duration-200"
+		  onClick={onClick}
+	    >
+		  {text}
+	    </button>
+  
+	);
+  }
+  export default Button;
