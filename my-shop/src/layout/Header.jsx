28204acf707d7685564041cfc3c779e572b8ae89
batu <batuhanbartuu@gmail.com>
2025-02-11 17:49:04 +0300

    commented

--- my-shop/src/layout/Header.jsx
+++ my-shop/src/layout/Header.jsx
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+// import React, { useState } from 'react';
+// import { useHistory } from 'react-router-dom';
 
-// Navbar bileşeni
-const Navbar = () => {
-// Menü açık/kapalı durumu için state tanımlama
-const [isMenuOpen, setIsMenuOpen] = useState(false);
-// Sayfa yönlendirme için useHistory hook'unu kullanma
-const history = useHistory();
-// Menü öğelerine tıklandığında yönlendirme yapma fonksiyonu
-const handleNavigation = (path) => {
-	setIsMenuOpen(false); // Menü kapat
-	history.push(path); // Belirtilen sayfaya yönlendir
-    };
+// // Navbar bileşeni
+// const Navbar = () => {
+// // Menü açık/kapalı durumu için state tanımlama
+// const [isMenuOpen, setIsMenuOpen] = useState(false);
+// // Sayfa yönlendirme için useHistory hook'unu kullanma
+// const history = useHistory();
+// // Menü öğelerine tıklandığında yönlendirme yapma fonksiyonu
+// const handleNavigation = (path) => {
+// 	setIsMenuOpen(false); // Menü kapat
+// 	history.push(path); // Belirtilen sayfaya yönlendir
+// 	};
+// };
 
 function Header() {
 
 	return (
-	    <main>
+		<main>
 		  <div>
 			<p>Bandage</p>
 			
 		  </div>
-	    </main>
+		</main>
 	);
-  }
-  export default Header;
+}
+
+export default Header;
