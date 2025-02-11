a53677ee54e4cacd13750fe980516a3514f683d1
batu <batuhanbartuu@gmail.com>
2025-02-11 16:38:56 +0300

    sosyal ikonlar resmi ekledndi css yazıldı

--- my-shop/src/layout/Footer.jsx
+++ my-shop/src/layout/Footer.jsx
@@ -1,10 +1,12 @@
 import Button from '../components/Button';
 
 function Footer() {
-
+	const handleClick = () => {
+		alert('You clicked me!');
+	};
 	return (
-
-
-	)
-}
-export default Footer;
+		<main className='text-left flex flex-col justify-center'>
+		    <div>
+			  <h3 className='text-h3'>Bendage</h3>
+			  <img src="/images/social-image.png" alt="social icons" />
+		    </div>
