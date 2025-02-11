4bf2fbf7076b09f7edf86620ea62717eab6caacb
batu <batuhanbartuu@gmail.com>
2025-02-11 16:33:15 +0300

    button css ve func eklendi

--- my-shop/src/componets/Button.jsx
+++ my-shop/src/componets/Button.jsx
@@ -1,7 +1,14 @@
 function Button({text, onClick}) {
+
 	return (
-
-
+  
+	    <button
+		  className="bg-[#23A6F0] text-white  py-4 px-8  transition-transform duration-200"
+		  onClick={onClick}
+	    >
+		  {text}
+	    </button>
+  
 	);
-}
-export default Button;
+  }
+  export default Button;
