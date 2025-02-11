a713f1531ad72ed41984df194ead946b29be9cf3
batu <batuhanbartuu@gmail.com>
2025-02-11 17:48:43 +0300

    bug fix geri donup bakılıcak

--- my-shop/src/App.jsx
+++ my-shop/src/App.jsx
@@ -1,9 +1,17 @@
-import './App.css'
+import './reset.css'
+import HomePage from './page/HomePage'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
+  
+
   return (
     <>
-      
+    <Switch>
+    <Route path="/" exact>
+      <HomePage/>
+      </Route>
+      </Switch>
     </>
   )
 }
