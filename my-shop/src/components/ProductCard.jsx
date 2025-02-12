b3606ace1a1929a6aa3f37928557c74e3b10ea70
batu <batuhanbartuu@gmail.com>
2025-02-12 15:04:19 +0300

    Ürün kartı bileşeni - Her bir ürün için başlık, alt başlık, fiyat ve resim içeren bir kart oluşturur.

--- my-shop/src/components/ProductCard.jsx
+++ my-shop/src/components/ProductCard.jsx
@@ -1,1 +1,19 @@
 import React, { useState } from 'react';
+// Ürün kartı bileşeni - Her bir ürün için başlık, alt başlık, fiyat ve resim içeren bir kart oluşturur.
+const ProductCard = ({ title, subtitle, price, image }) => {
+	return (
+	  <section>
+	    <div className='flex flex-col flex-wrap'>
+		{/* Ürün resmi */}
+		<img src={image} alt="ProductCard" />
+		
+		{/* Ürün bilgileri */}
+		<div className="p-4 text-center">
+		  <h5 className="text-h5 font-bold">{title}</h5>
+		  <h6 className="text-h6 text-secondText">{subtitle}</h6>
+		  <p className="text-h6">{price}</p>
+		</div>
+	    </div>
+	  </section>
+	);
+    };
