62e65544ae15819752cf0b0be06c33f80dd2e245
batu <batuhanbartuu@gmail.com>
2025-02-12 15:40:12 +0300

    Harici bir bağlantıya yönlendiren kapsayıcı (yeni sekmede açılması için target="_blank" kullanıldı)

--- my-shop/src/components/IconCard.jsx
+++ my-shop/src/components/IconCard.jsx
@@ -3,4 +3,16 @@ import React from 'react';
 // Tek bir ikon kartını temsil eden bileşen
 const IconCard = ({ imageSrc, link, altText }) => {
 	return (
-		
+		// Harici bir bağlantıya yönlendiren kapsayıcı (yeni sekmede açılması için target="_blank" kullanıldı)
+		<a href={link} target="_blank" rel="noopener noreferrer">
+		<div className="flex justify-center items-center">
+		  {/* İkon resmi - Grayscale filtresiyle gri tonlarda gösteriliyor */}
+		  <img
+		    src={imageSrc}
+		    alt={altText}
+		    className="filter grayscale"
+		  />
+		</div>
+	    </a>
+	  );
+	};
