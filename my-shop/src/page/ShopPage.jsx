a0f0b97922149f2ec883d016c5a1288eca0e86d7
batu <batuhanbartuu@gmail.com>
2025-02-12 17:12:28 +0300

    shop page sarmalı

---
+++ my-shop/src/page/ShopPage.jsx
@@ -0,0 +1,18 @@
+import CategoryCard from "../components/CategoryCard";
+import IconList from "../components/IconCard";
+import Footer from "../layout/Footer";
+import Navbar from "../layout/Header";
+
+function ShopPage() {
+
+    return (
+        <>
+        <Navbar />
+        <CategoryCard/>
+        <IconList/>
+        <Footer/>
+        </>
+    );
+}
+
+export default ShopPage;
