6da4572ff7c3053e1b047636bd430db106f40cb2
batu <batuhanbartuu@gmail.com>
2025-02-13 21:58:32 +0300

    homepage güncellendi

--- my-shop/README.md
+++ my-shop/README.md
@@ -1,10 +1,48 @@
-# React + Vite
+---
+ProjectName: E-commerce
+Development Process Time: '2024-07-02 / 2024-07-03'
+spoiler: Dynamic With React & Spring
+FE, BE: ["React", "Spring"]
+githubUsername: 'erayerm'
+---
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+<!-- Greeting -->
+# Hey! 👋
 
-Currently, two official plugins are available:
+### 🔗  <code><a href="https://ecommerce-iota-beige.vercel.app/" target="_blank"> Vercel Deployment </a></code>
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Getting Started 🖱️
+To get started  you can simply clone this `ecommerce` repository and install the dependencies.
 
-# deneme
+Clone the `ecommerce` repository using git:
+
+```bash
+git clone https://github.com/erayerm/ecommerce.git
+cd ecommerce
+```
+
+Install dependencies with this command:
+```bash
+npm install
+```
+
+Run the application with this command:
+```bash
+npm run dev
+or
+yarn dev
+```
+
+Use them for login: `customer@commerce.com` - `123456`
+
+</p>
+
+<h1>About Developer</h1>
+
+![erayerm's Stats](https://github-readme-stats.vercel.app/api?username=erayerm&theme=vue-dark&show_icons=true&hide_border=true&count_private=true)
+<br/>
+![erayerm's Top Languages](https://github-readme-stats.vercel.app/api/top-langs/?username=erayerm&theme=vue-dark&show_icons=true&hide_border=true&layout=compact)
+<h4 className="flex justify-center align-middle">Thanks for Visiting!</h4>
+
+- 📫 How to reach me:
+<code><a href="mailto:ermemiseray@gmail.com" target="_blank">Via Email 📫 </a></code>
--- my-shop/src/page/HomePage.jsx
+++ my-shop/src/page/HomePage.jsx
@@ -1,107 +1,33 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import IconList from '../components/IconCard';
-import CardList from '../components/Card';
-import Footer from '../layout/Footer';
-import Navbar from '../layout/Header';
-import PostCardList from '../components/PostCard';
-import ProductCardList from '../components/ProductCardList';
-import { fetchProducts } from '../actions/productsActions';
-import { TopBanner } from '../layout/Banner';
+import BestSellerProducts from "../components/BestsellerProducts"
+import CarouselMainFirst from "../components/CarouselMainFirst"
+import CarouselMainSecond from "../components/CarouselMainSecond"
+import EditorsPick from "../components/EditorsPick"
+import FeaturedPosts from "../components/FeaturedPosts"
+import PartOfTheNeuralUniverse from "../components/PartOfTheNeuralUniverse"
+import { firstCarouselContent, secondCarouselContent } from "../mock/carouselContentsData"
 
-function HomePage() {
-    const dispatch = useDispatch();
-    
-    // Redux store'dan ürünleri, yükleme durumunu ve hataları al
-    const { products, loading, error } = useSelector(state => state.products);
-
-    useEffect(() => {
-        // Sayfa yüklendiğinde ürünleri çekmek için Redux eylemini tetikle
-        dispatch(fetchProducts());
-    }, [dispatch]);
-
-    // Eğer veri yükleniyorsa ekrana "Loading..." mesajı bas
-    if (loading) return <div>Loading...</div>;
-    
-    // Eğer hata oluşursa hata mesajını ekrana bas
-    if (error) return <div>{error}</div>;
-
+export default function HomePage() {
     return (
-        <div>
-            {/* Sayfanın üst kısmındaki banner bileşeni */}
-            <TopBanner />
-            {/* Sayfanın üst navigasyon çubuğu */}
-            <Navbar />
-
-            {/* Özel bir arka planla yaz sezonunu tanıtan bir bölüm */}
-            <section className='bg-custom-gradient text-center  rounded-[20px] mx-2 px-12 pt-24 sm:pt-0 sm:flex md:mx-16'>
-                <div className="p-16 sm:flex sm:flex-col sm:justify-center text-center md:text-left">
-                    <h5 className='text-blue text-h5 font-bold sm:text-h6'>SUMMER 2020</h5>
-                    <h2 className='text-primaryText md:text-h2 font-bold py-8 text-h4'>NEW COLLECTION</h2>
-                    <h4 className='text-h4 text-secondText'>We know how large objects will act, but things on a small scale.</h4>
-                    <div className='py-8'>
-                        {/* Kullanıcıyı alışverişe yönlendiren buton */}
-                        <button className='md:text-h3 font-bold bg-blueText text-white rounded-lg px-8 py-3 sm:text-h5'>
-                            SHOP NOW
-                        </button>
+        <>
+            <section className='w-screen bg-primary-blue'>
+                <div className='max-w-[1440px] mx-auto'>
+                    <div className="w-full aspect-[144/85] sm:aspect-[41/75] mx-auto" >
+                        <CarouselMainFirst slides={firstCarouselContent} />
                     </div>
                 </div>
-                <div className='flex justify-center sm:justify-end'>
-                    {/* Sayfada gösterilecek görsel */}
-                    <img src='/images/figure1.png' />
-                </div>
             </section>
 
-            {/* Sayfada kullanılacak ikon listesini gösteren bileşen */}
-            <IconList />
-            
-            {/* Kart şeklinde ürün bilgilerini içeren bileşen */}
-            <CardList />
-            
-            {/* Redux'tan alınan ürünleri listeleyen bileşen */}
-            <ProductCardList products={products} />
-
-            {/* Öne çıkan ürünleri tanıtan bölüm */}
-            <section className='md:flex gap-4 items-center'>
-                <div className='flex flex-col gap-2 p-16 font-bold md:w-1/2 md:p-24'>
-                    <h5 className='text-h5 text-blueText'>Featured Products</h5>
-                    <h2 className='text-h2'>We love what we do</h2>
-                    <p className='text-h6 text-secondText'>
-                        Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics
-                    </p>
-                    <p className='text-h6 text-secondText'>
-                        Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics
-                    </p>
-                </div>
-                <div className='flex md:w-1/2 px-3 justify-center h-auto'>
-                    {/* Öne çıkan ürünlerin görselleri */}
-                    <img src='/images/featured1.png' />
-                    <img src='/images/featured2.png' />
+            <EditorsPick />
+            {/*<BestSellerProducts />*/}
+            <section className='w-screen bg-[#23856D]'>
+                <div className='max-w-[1440px] mx-auto'>
+                    <div className="w-full aspect-[144/85] sm:aspect-[41/75] mx-auto" >
+                        <CarouselMainSecond slides={secondCarouselContent} />
+                    </div>
                 </div>
             </section>
-
-            {/* Hizmetleri tanıtan bölüm */}
-            <section className='py-20'>
-                <div className='text-center px-12 font-bold leading-loose'>
-                    <h4 className='text-h4 text-secondText'>Featured Products</h4>
-                    <h3 className='text-h3 font-bold'>THE BEST SERVICES</h3>
-                    <h6 className='text-h6 text-secondText'>Problems trying to resolve the conflict between</h6>
-                </div>
-                {/* Hizmetleri temsil eden ikonlar */}
-                <div className='flex flex-wrap justify-center gap-4 pt-20'>
-                    <img src='/images/icon1.png' alt='Easy Wins' />
-                    <img src='/images/icon2.png' alt='Concrete' />
-                    <img src='/images/icon3.png' alt='Hack Growth' />
-                </div>
-            </section>
-
-            {/* Blog veya haber içeriklerini gösteren bileşen */}
-            <PostCardList />
-
-            {/* Sayfanın alt kısmındaki footer bileşeni */}
-            <Footer />
-        </div>
-    );
+            <PartOfTheNeuralUniverse />
+            <FeaturedPosts />
+        </>
+    )
 }
-
-export default HomePage;
