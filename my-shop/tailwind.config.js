89942ca017dee2eab7c4f5f0dd578035dd67afbc
batu <batuhanbartuu@gmail.com>
2025-02-13 15:07:36 +0300

    npm audit fix

--- my-shop/tailwind.config.js
+++ my-shop/tailwind.config.js
@@ -1,29 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-	  "./index.html",
-	  "./src/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-	  extend: {
-	    colors: {
-		background: '#FFFFFF',
-		primaryText:'#252B42',
-		secondText: '#737373',
-		blueText: '#23A6F0'
-	    },
-	    fontFamily: {
-		montserrat: ['Montserrat','sans-serif'],
-	    },
-	    fontSize: {
-		'h5': '1rem',
-		'h2': '2.5rem',
-		'h1': '3.5rem',
-		'h3': '1.5rem',
-		'h4': '1.25rem',
-		'h6': '0.875rem'
-	    }
-	  },
-	},
-	plugins: [],
-    }
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to right, #67e8f9, #5eead4)', // from-cyan-300 to-teal-200
+      },
+      colors: {
+        background: '#FFFFFF',
+        primaryText:'#252B42',
+        secondText: '#737373',
+        blueText: '#23A6F0'
+      },
+      fontFamily: {
+        montserrat: ['Montserrat','sans-serif'],
+      },
+      fontSize: {
+        'h5': '1rem',
+        'h2': '2.5rem',
+        'h1': '3.5rem',
+        'h3': '1.5rem',
+        'h4': '1.25rem',
+        'h6': '0.875rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
