2a3f3b26c82b388be052f315f63aaad20e015d83
batu <batuhanbartuu@gmail.com>
2025-02-14 23:17:59 +0300

    2

--- my-shop/src/main.jsx
+++ my-shop/src/main.jsx
@@ -1,17 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import store from "./redux/store";
+import store from "./store/store.js";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </StrictMode>
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
--- my-shop/tailwind.config.js
+++ my-shop/tailwind.config.js
@@ -1,42 +1,69 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class"],
+  darkMode: ["selector", "class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+
       colors: {
-        primary: "#23A6F0",
-        secondary: "#23856D",
-        muted: "#BDBDBD",
-        success: "#2DC071",
-        lightblue: "#8EC2F2",
-        red: "#E74040",
-        lightGray: "#E6E6E6",
-        yellow: "#F3CD03",
-        lightbg: "#FAFAFA",
-        text: {
-          default: "#252B42",
-          secondary: "#737373",
-          light: "#FFFFFF",
-          lightSecondary: "#858585",
+        navy: "#17213C",
+        "dark-green": "#23856D",
+        olive: "#176552",
+        laurel: "#39603D",
+        "dark-gray": "#3C403D",
+        crimson: "#B73225",
+        cerulean: "#004E7C",
+        indigo: "#283747",
+        "poppy-red": "#E63946",
+        "linoleum-blue": "#457B9D",
+        "nile-blue": "#1D3557",
+        "mango-orange": "#FF7B47",
+        "deep-sea-green": "#0D5C63",
+        "yankees-blue": "#17213C",
+        "squash-orange": "#FCA311",
+        "dark-navy": "#14213D",
+        sunburst: "#FFA62B",
+        "pacific-blue": "#00A1C1",
+        "sapphire-blue": "#16697A",
+        "success-color": "#2DC071",
+        "light-gray": "#737373",
+        "primary-color": "#23A6F0",
+        "secondary-color": "#23856D",
+        "alert-color": "#E77C40",
+        "danger-color": "#E74040",
+        "text-color": "#252B42",
+        gray: "#FAFAFA",
+        "hover-color": "#2A7CC7",
+      },
+      maxWidth: {
+        "75vw": "75vw",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
         },
       },
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: "1rem",
-          sm: "1.5rem",
-          md: "2rem",
-          lg: "2.5rem",
-          xl: "3rem",
-        },
-        screens: {
-          sm: "540px",
-          md: "720px",
-          lg: "920px",
-          xl: "1140px",
-          "2xl": "1440px",
-        },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
