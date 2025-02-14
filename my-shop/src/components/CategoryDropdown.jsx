b1dc697b8426d0ac49a14f0366c1e0742eb7177c
batu <batuhanbartuu@gmail.com>
2025-02-14 21:39:03 +0300

    Açılır menü tetikleyicisi ve   Açılır menü öğesi eklenıd Redux aksiyonu

--- my-shop/src/components/CategoryDropdown.jsx
+++ my-shop/src/components/CategoryDropdown.jsx
@@ -1,28 +1,24 @@
-import { useSelector, useDispatch } from "react-redux"; // Redux ile state yönetimi
-import { Link } from "react-router-dom"; // Sayfa yönlendirme
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import {
-  DropdownMenu, // Açılır menü bileşeni
-  DropdownMenuContent, // Açılır menü içeriği
-  DropdownMenuItem, // Açılır menü öğesi
-  DropdownMenuTrigger, // Açılır menü tetikleyicisi
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ChevronDown } from "lucide-react"; // Aşağı ok simgesi
-import createSlug from "../utils/createSlug"; // URL dostu slug oluşturmak için
-import { updateCategory } from "../store/actions/productActions"; // Redux aksiyonu
+import { ChevronDown } from "lucide-react";
+import createSlug from "../utils/createSlug";
+import { updateCategory } from "../store/actions/productActions";
 
-// Kategori Dropdown Menüsü
 const CategoryDropdown = () => {
-  const dispatch = useDispatch(); // Redux aksiyonlarını çağırmak için
+  const dispatch = useDispatch();
 
-  // Kategoriye tıklanınca Redux state'ini günceller
   const handleCategoryClick = (categoryId) => {
     dispatch(updateCategory(categoryId));
   };
 
-  // Redux store'dan kategorileri alır
   const categories = useSelector((state) => state.product.categories);
 
-  // Kadın ve erkek kategorilerini ayır
   const femaleCategories = categories.filter(
     (category) => category.gender === "k"
   );
@@ -32,25 +28,23 @@ const CategoryDropdown = () => {
 
   return (
     <div className="inline-flex items-center justify-center text-center space-x-0 text-light-gray">
-      {/* Mağaza Ana Sayfasına Link */}
       <Link to="/shop" className="ml-4 font-semibold">
         Shop
       </Link>
-
-      {/* Dropdown Menü */}
+      {}
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <ChevronDown className="mt-1" /> {/* Açılır menü simgesi */}
+          <ChevronDown className="mt-1" />
         </DropdownMenuTrigger>
         <DropdownMenuContent className="p-4 grid grid-cols-2 gap-4">
-          
-          {/* Kadın Kategorileri */}
           <div>
             <h4 className="font-semibold text-dark-gray mb-2">Kadın</h4>
             {femaleCategories.map((category) => (
               <DropdownMenuItem key={category.id} asChild>
                 <Link
-                  to={`/shop/kadin/${createSlug(category.title)}/${category.id}`}
+                  to={`/shop/kadin/${createSlug(category.title)}/${
+                    category.id
+                  }`}
                   className="text-dark-gray"
                   onClick={() => handleCategoryClick(category.id)}
                 >
@@ -59,14 +53,14 @@ const CategoryDropdown = () => {
               </DropdownMenuItem>
             ))}
           </div>
-
-          {/* Erkek Kategorileri */}
           <div>
             <h4 className="font-semibold text-dark-gray mb-2">Erkek</h4>
             {maleCategories.map((category) => (
               <DropdownMenuItem key={category.id} asChild>
                 <Link
-                  to={`/shop/erkek/${createSlug(category.title)}/${category.id}`}
+                  to={`/shop/erkek/${createSlug(category.title)}/${
+                    category.id
+                  }`}
                   className="text-dark-gray"
                   onClick={() => handleCategoryClick(category.id)}
                 >
@@ -75,11 +69,9 @@ const CategoryDropdown = () => {
               </DropdownMenuItem>
             ))}
           </div>
-
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
   );
 };
-
 export default CategoryDropdown;
